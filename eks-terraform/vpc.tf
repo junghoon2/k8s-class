0a4337581543098087bc7c5657f89646f445a8c1
erdia22 <erdia22@gmail.com>
2023-05-25 08:54:15 +0900

    modify karpenter config

--- eks-terraform/vpc.tf
+++ eks-terraform/vpc.tf
@@ -19,10 +19,12 @@ module "vpc" {
 
   public_subnet_tags = {
     "kubernetes.io/role/elb" = 1
+    "karpenter.sh/discovery" = local.name
   }
 
   private_subnet_tags = {
     "kubernetes.io/role/internal-elb" = 1
+    "karpenter.sh/discovery"          = local.name
   }
 
   tags = local.tags
