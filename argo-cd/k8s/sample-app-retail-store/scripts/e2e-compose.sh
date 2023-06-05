8c2c6d1e725b273eaa17ca01adc032e7c9175ce1
erdia22 <erdia22@gmail.com>
2023-06-05 14:04:04 +0900

    change directory name

--- argo-cd/k8s/sample-app-retail-store/scripts/e2e-compose.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-cd $DIR/../src/e2e && bash ./scripts/run-docker.sh -n docker-compose_default 'http://ui:8080'
--- argo-cd/k8s/utils/get-all-eks-version.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for context in $(kubectl config get-contexts -o=name); do
-  echo "Context: $context"
-  kubectl version --output=json --context="$context"|jq  '.serverVersion'
-  echo ""
-done
