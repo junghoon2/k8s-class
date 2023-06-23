b473302129b0bfbe51fcff8cccb7449378ec7237
erdia22 <erdia22@gmail.com>
2023-06-23 13:47:09 +0900

    add irsa for external dns

--- eks-terraform/irsa-role.tf
+++ eks-terraform/irsa-role.tf
@@ -13,3 +13,20 @@ module "load_balancer_controller_irsa_role" {
 
   tags = local.tags
 }
+
+module "external_dns_irsa_role" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+
+  role_name                     = "external-dns"
+  attach_external_dns_policy    = true
+  external_dns_hosted_zone_arns = ["arn:aws:route53:::hostedzone/XXXXXXXXX"]
+
+  oidc_providers = {
+    ex = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:external-dns"]
+    }
+  }
+
+  tags = local.tags
+}
