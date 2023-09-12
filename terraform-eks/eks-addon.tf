28cbf31e18134d79f2044eb3c9e6970e600605f3
jerry <erdia22@gmail.com>
2023-09-13 05:02:28 +0900

    fix: modify ebs csi driver

--- terraform-eks/eks-addon.tf
+++ terraform-eks/eks-addon.tf
@@ -5,13 +5,7 @@ variable "aws_account_id" {
 resource "aws_eks_addon" "aws_ebs_csi_driver" {
   cluster_name             = module.eks.cluster_name
   addon_name               = "aws-ebs-csi-driver"
-  service_account_role_arn = "arn:aws:iam::${var.aws_account_id}:role/ebs-csi-switch-singapore-test"
-  addon_version            = "v1.20.0-eksbuild.1"
+  service_account_role_arn = "arn:aws:iam::${var.aws_account_id}:role/ebs-csi-switch-jerry-test"
+  addon_version            = "v1.22.0-eksbuild.2"
   resolve_conflicts        = "OVERWRITE"
-
-  lifecycle {
-    ignore_changes = [
-      service_account_role_arn
-    ]
-  }
 }
