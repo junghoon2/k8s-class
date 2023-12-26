b8ed307c4acd1bbe7cfb098b8e0ab07bdac9c1c7
jerry <erdia22@gmail.com>
2023-12-26 13:59:59 +0900

    feat: modify tf code for iam role

--- terraform/iam/loki-s3/main.tf
+++ terraform/iam/loki-s3/main.tf
@@ -26,7 +26,24 @@ resource "aws_iam_policy" "AWSS3EksLokiAccess" {
 }
 
 resource "aws_iam_role" "loki_jerry_test" {
-  assume_role_policy    = "{\"Statement\":[{\"Action\":\"sts:AssumeRoleWithWebIdentity\",\"Condition\":{\"StringEquals\":{\"oidc.eks.ap-northeast-2.amazonaws.com/id/${ENDPOINT}:aud\":\"sts.amazonaws.com\",\"oidc.eks.ap-northeast-2.amazonaws.com/id/${ENDPOINT}:sub\":\"system:serviceaccount:monitoring:loki\"}},\"Effect\":\"Allow\",\"Principal\":{\"Federated\":\"arn:aws:iam::${ACCOUNT_ID}:oidc-provider/oidc.eks.ap-northeast-2.amazonaws.com/id/${ENDPOINT}\"}}],\"Version\":\"2012-10-17\"}"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Federated = "arn:aws:iam::${ACCOUNT_ID}:oidc-provider/oidc.eks.ap-northeast-2.amazonaws.com/id/${ENDPOINT}"
+        },
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "oidc.eks.ap-northeast-2.amazonaws.com/id/${ENDPOINT}:aud" = "sts.amazonaws.com"
+            "oidc.eks.ap-northeast-2.amazonaws.com/id/${ENDPOINT}:sub" = "system:serviceaccount:monitoring:loki"
+          }
+        }
+      },
+    ]
+  })
   description           = "loki role for jerry-test eks"
   force_detach_policies = false
   managed_policy_arns   = ["arn:aws:iam::${ACCOUNT_ID}:policy/AWSS3EksLokiAccess"]
