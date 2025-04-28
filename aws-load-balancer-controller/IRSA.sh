093e75fc7850c9cfe798ad0e5e9d6b9177458975
jerry <erdia22@gmail.com>
2025-04-28 09:27:59 +0900

    feat: aws lb controller configs

--- aws-load-balancer-controller/IRSA.sh
+++
@@ -1,8 +0,0 @@
-eksctl create iamserviceaccount \
-  --cluster=my-cluster \
-  --namespace=kube-system \
-  --region=ap-northeast-2 \
-  --name=aws-load-balancer-controller \
-  --role-name AmazonEKSLoadBalancerControllerRole \
-  --attach-policy-arn=arn:aws:iam::123456789012:policy/AWSLoadBalancerControllerIAMPolicy \
-  --approve
